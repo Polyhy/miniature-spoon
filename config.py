77e7fd5de6320c353147f90d741f2d4b9575edfa
polyhy <polyhy.gwon@gmail.com>
2016-02-18 16:57:56 +0900

    Update config.py edit SQLALCHEMY setting

--- config.py
+++ config.py
@@ -6,5 +6,11 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = "hellominiaturespoon"
 
 # SQLALCHEMY
-SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost/miniature_spoon'
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:tkdaud1004@localhost/miniature_spoon'
+DATABASE_USER_ID = 'root'
+DATABASE_USER_PASSWD = 'tkdaud1004'
+DATABASE_NAME = 'miniature_spoon'
+DATABASE_PORT = 3306
+DATABASE_ADDR = 'localhost'
+SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@%s/%s' % (DATABASE_USER_ID, DATABASE_USER_PASSWD, DATABASE_ADDR, DATABASE_NAME)
 DATABASE_CONNECT_OPTIONS = {}
