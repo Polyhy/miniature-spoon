a6b2bd84b0ec6435cf64c25055154c1bfb561c6a
polyhy <polyhy.gwon@gmail.com>
2016-02-20 04:17:39 +0900

    Reformat code

--- miniature_spoon_app/link/views.py
+++ miniature_spoon_app/link/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, request, flash, g, session, redirect, url_for
-
+from miniature_spoon_app import db
+from miniature_spoon_app.link.model import Link
 
 links = Blueprint('links', __name__, url_prefix='/v1/link')
+
+
 @links.route('/', methods=['GET'])
 def test():
     return "work"
