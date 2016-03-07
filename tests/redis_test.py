b78c9628b82847e58fa34818e6338ad73c07b75b
polyhy <polyhy.gwon@gmail.com>
2016-03-08 00:43:31 +0900

    Update redis test

--- tests/redis_test.py
+++ tests/redis_test.py
@@ -5,5 +5,8 @@ from miniature_spoon_app import redis_conn
 
 class RedisTest(unittest.TestCase):
     def test_redis_connection(self):
+        self.assertEqual(redis_conn.get("test"), None)
         redis_conn.set("test", 1)
-        print redis_conn.get("test")
+        self.assertEqual(redis_conn.get("test"), '1')
+        redis_conn.delete("test")
+        self.assertEqual(redis_conn.get("test"), None)
