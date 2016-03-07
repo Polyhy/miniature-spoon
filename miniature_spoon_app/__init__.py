edbead859d5e56ce781010062ca04650ce1a1ff0
polyhy <polyhy.gwon@gmail.com>
2016-03-08 01:21:00 +0900

    Remove useless import

--- miniature_spoon_app/__init__.py
+++ miniature_spoon_app/__init__.py
@@ -1,11 +1,9 @@
-import redis
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import config
 
-from miniature_spoon_app.config import REDIS_HOST, REDIS_PORT, REDIS_DB
+import config
 
 app = Flask(__name__)
 
@@ -14,7 +12,6 @@ engine = create_engine(config.SQLALCHEMY_DATABASE_URI, echo=True)
 SessionFactory = sessionmaker(autocommit=False, autoflush=True, bind=engine,
                               expire_on_commit=False)
 Base = declarative_base()
-# redis_conn = redis.StrictRedis(REDIS_HOST, REDIS_PORT, REDIS_DB)
 
 from miniature_spoon_app.link.views import links as linkModule
 import miniature_spoon_app.views
