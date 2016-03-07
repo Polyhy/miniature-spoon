4829543f3b943d4266ccabf12f6247892e631de5
polyhy <polyhy.gwon@gmail.com>
2016-03-08 00:44:57 +0900

    Update redis test

--- miniature_spoon_app/__init__.py
+++ miniature_spoon_app/__init__.py
@@ -14,7 +14,7 @@ engine = create_engine(config.SQLALCHEMY_DATABASE_URI, echo=True)
 SessionFactory = sessionmaker(autocommit=False, autoflush=True, bind=engine,
                               expire_on_commit=False)
 Base = declarative_base()
-redis_conn = redis.StrictRedis(REDIS_HOST, REDIS_PORT, REDIS_DB)
+# redis_conn = redis.StrictRedis(REDIS_HOST, REDIS_PORT, REDIS_DB)
 
 from miniature_spoon_app.link.views import links as linkModule
 import miniature_spoon_app.views
--- tests/redis_test.py
+++ tests/redis_test.py
@@ -1,10 +1,11 @@
 import unittest
-
-from miniature_spoon_app import redis_conn
+import redis
+from miniature_spoon_app.config import  REDIS_HOST, REDIS_PORT, REDIS_DB
 
 
 class RedisTest(unittest.TestCase):
     def test_redis_connection(self):
+        redis_conn = redis.StrictRedis(REDIS_HOST, REDIS_PORT, REDIS_DB)
         self.assertEqual(redis_conn.get("test"), None)
         redis_conn.set("test", 1)
         self.assertEqual(redis_conn.get("test"), '1')
